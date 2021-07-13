948eec5d781f10ba9859289ada814069715135ae
bodinet <bodinet@tgen.org>
2021-07-13 10:30:00 -0700

    Version 1.0.7 Debugging option to use cram/sam files

--- main.py
+++ main.py
@@ -515,7 +515,7 @@ def writeGTF(inGTF, file_path):
 print(ref_fasta)
 print(input_aln)
 in_bam = read_aln_file(input_aln, ref_fasta)
-sys.exit()
+
 # Case where user wants to build an IG GTF from a different GTF than provided. In this case, the program builds
 # the GTF and then processes the input BAM using the new GTF
 if in_gtf is not None and build is True:
